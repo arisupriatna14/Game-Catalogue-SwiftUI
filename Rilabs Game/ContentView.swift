b72bcfd846378409df32df979bb6ecf39c256631
arisupriatna14 <arisupriatna703@gmail.com>
2020-07-01 21:17:11 +0700

    Initial Commit

---
+++ Rilabs Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rilabs Game
+//
+//  Created by Ari Supriatna on 01/07/20.
+//  Copyright © 2020 Ari Supriatna. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
